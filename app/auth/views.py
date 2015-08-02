9d9951745afee7b22f632889cedaef4abb8c31f1
caser <jiao.xuejiao@gmail.com>
2015-08-02 16:14:03 +0800

    account confirmation

--- app/auth/views.py
+++ app/auth/views.py
@@ -44,6 +44,49 @@ def register():
                     username=form.username.data,
                     password=form.password.data)
         db.session.add(user)
-        flash('感谢您的注册，请登录！')
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, u'请验证您的邮箱',
+                   'auth/email/confirm', user=user, token=token)
+        flash(u'请到注册使用的邮箱中验证您的账户')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+# 验证 用户点击邮箱中的链接验证
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash(u'您的账号已验证')
+    else:
+        flash(u'验证链接无效或者已过期，请重新验证')
+    return redirect(url_for('main.index'))
+
+# 验证 重新发送验证URL
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, u'请验证您的账号',
+               'auth/email/confirm', user=current_user, token=token)
+    flash(u'验证邮件已经发送至您注册使用的邮箱')
+    return redirect(url_for('main.index'))
+
+
+# 注册但是未验证的用户 只能访问 auth. 或者 static 或者 unconfirmed页面
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated() \
+        and not current_user.confirmed \
+        and request.endpoint[:5] != 'auth.' \
+        and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+# 未验证用户和非匿名用户只能访问 unconfirmed页面
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous() or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed')
--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 # 处理用户登录
 from flask.ext.login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 """
 单独的数据库模块
@@ -27,6 +29,7 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
 
     # 储存密码的hash码
     password_hash = db.Column(db.String(128))
@@ -47,6 +50,24 @@ class User(db.Model):
     # 密码 验证真伪
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # 验证 生成验证码
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    # 验证 验证 验证码
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.sessionl.add(self)
+        return True
 
 # 根据用户ID得到用户对象
 # 登录
