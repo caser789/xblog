a00215396dbb97ef7a2d450111a79b1fd415e177
caser <jiao.xuejiao@gmail.com>
2015-08-15 15:45:26 +0800

    enable ssl

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,8 @@ pagedown = PageDown()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+
 # config_name: 配置名，对应与config中的配置dict中的key
 def create_app(config_name):
     app  = Flask(__name__)
@@ -45,5 +47,9 @@ def create_app(config_name):
     # 设置 蓝图 对应的 URL 前缀
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+        from flask.ext.sslify import SSLify
+        sslify = SSLify(app)
 
     return app
