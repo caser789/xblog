c980aa4936505b83a8f6e2f20dd6598fde81b832
caser <jiao.xuejiao@gmail.com>
2015-08-16 16:11:37 +0800

    turn off auto teardown

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "xuejiao's blog"
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT =  587
     MAIL_USE_TLS = True 
